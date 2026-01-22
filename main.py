111ebdf623210ec0c104d4b92d7a4e1977321733
akunaku123buat-cyber <akunaku123.buat@gmail.com>
2026-01-22 19:14:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ async def jalankan_otomatis():
     # Ambil Kunci dari Secrets GitHub
     api_key = os.getenv("GEMINI_API_KEY")
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-2.5-flash')
 
     # 1. Buat Naskah
     print("✍️ Membuat naskah...")
